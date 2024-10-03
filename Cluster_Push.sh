051e3f8d03f9b5ea373533498e50bf817a449e81
CarlosRauert <carlos.rauert@charite.de>
2024-10-03 16:00:13 +0200

    Changes of 2024-10-03

--- Cluster_Push.sh
+++ Cluster_Push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 token=$(head -n 1 $HOME/work/dir/token.txt)
 cd $HOME/work/Scripts_Git_Repos/Spatial_Scripts
+git add .
+DATE=$(date +"%Y-%m-%d")
+git commit -m "Changes of ${DATE}"
 git push https://${token}@github.com/CarlosRauert/Spatial_Scripts main
 
 cd $HOME/work/Scripts_Git_Repos/Genomics_Scripts
