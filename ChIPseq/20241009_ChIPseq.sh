c989708c265cc00076bc69b211e4cb2f7b08c9da
CarlosRauert <carlos.rauert@charite.de>
2024-10-14 11:49:38 +0200

    efwfw

--- ChIPseq/20241009_ChIPseq.sh
+++ ChIPseq/20241009_ChIPseq.sh
@@ -1,8 +1,12 @@
 conda activate NF_CS
 cd /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
 
-export NXF_APPTAINER_CACHEDIR=/data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
-export NXF_SINGULARITY_CACHEDIR=/data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
+ln -s scratch/ChIPseq/cache_Files/.apptainer ~/.apptainer 
+ln -s scratch/ChIPseq/cache_Files/.nextflow ~/.nextflow
+ln -s scratch/ChIPseq/cache_Files/.local ~/.local
+-c /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/ChIPseq/20241014_edit_bih.config
 
+export NXF_APPTAINER_CACHEDIR=/data/cephfs-1/work/projects/dubois-lpwgs/singularity
+export NXF_SINGULARITY_CACHEDIR=/data/cephfs-1/work/projects/dubois-lpwgs/singularity
 nextflow run nf-core/chipseq -profile test --outdir /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq -c /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/ChIPseq/20241014_edit_bih.config
 
