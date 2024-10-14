66255f13d7848c2b8c118f12437109168c9113f2
CarlosRauert <carlos.rauert@charite.de>
2024-10-14 14:04:17 +0200

    q3tq3t

--- ChIPseq/20241009_ChIPseq.sh
+++ ChIPseq/20241009_ChIPseq.sh
@@ -8,5 +8,6 @@ ln -s scratch/ChIPseq/cache_Files/.local ~/.local
 
 export NXF_APPTAINER_CACHEDIR=/data/cephfs-1/work/projects/dubois-lpwgs/singularity
 export NXF_SINGULARITY_CACHEDIR=/data/cephfs-1/work/projects/dubois-lpwgs/singularity
+# test
 nextflow run nf-core/chipseq -profile test --outdir /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq -c /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/ChIPseq/20241014_edit_bih.config
-
+# run with sample sheet
--- ChIPseq/20241009_Prefetch_SRA_List.sh
+++ ChIPseq/20241009_Prefetch_SRA_List.sh
@@ -1,3 +1,14 @@
-for file in /data/cephfs-1/home/users/rauertc_c/scratch/ChIPseq/20241009_Acc_Lists*; do
-    prefetch --option-file ${file} --output-directory /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq/SRA
+for file in /data/cephfs-1/home/users/rauertc_c/scratch/ChIPseq/20241009_Acc_Lists/*; do
+    if [ -f "$file" ]; then
+        echo $file
+        prefetch --option-file ${file} --output-directory /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq/SRA 
+    fi
+done
+
+cd /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq/FastQ
+
+for dir in /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq/SRA/*
+do
+    echo $dir
+    fasterq-dump $dir
 done
