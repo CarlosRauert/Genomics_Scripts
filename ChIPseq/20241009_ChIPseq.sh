024a3d1c2ca024f64675505e9b124d891c1e0e35
CarlosRauert <carlos.rauert@charite.de>
2024-10-14 14:15:20 +0200

    dvda

--- ChIPseq/20241009_ChIPseq.sh
+++ ChIPseq/20241009_ChIPseq.sh
@@ -11,3 +11,4 @@ export NXF_SINGULARITY_CACHEDIR=/data/cephfs-1/work/projects/dubois-lpwgs/singul
 # test
 nextflow run nf-core/chipseq -profile test --outdir /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq -c /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/ChIPseq/20241014_edit_bih.config
 # run with sample sheet
+nextflow run nf-core/chipseq --sampleoutdir /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq/20241014_NF_CS_Out -c /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/ChIPseq/20241014_edit_bih.config
