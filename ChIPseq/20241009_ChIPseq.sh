206bedce4ffe9bb7db66bb3b60d3837ed028f1c5
CarlosRauert <carlos.rauert@charite.de>
2024-10-14 11:04:12 +0200

    wddw

--- ChIPseq/20241009_ChIPseq.sh
+++ ChIPseq/20241009_ChIPseq.sh
@@ -4,5 +4,5 @@ cd /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
 export NXF_APPTAINER_CACHEDIR=/data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
 export NXF_SINGULARITY_CACHEDIR=/data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
 
-nextflow run nf-core/chipseq -profile test,bih --outdir /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq
+nextflow run nf-core/chipseq -profile test --outdir /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/ChIPseq -c /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/ChIPseq/20241014_edit_bih.config
 
