4438d4a606364aa7c2f68ade955555b1a21f0b45
CarlosRauert <carlos.rauert@charite.de>
2024-11-18 17:05:09 +0100

    efwfw

---
+++ Github_Proxy.sh
@@ -0,0 +1,5 @@
+git config --global http.proxy http://proxy.charite.de:8080
+git config --global https.proxy http://proxy.charite.de:8080
+
+git config --global --unset http.proxy
+git config --global --unset https.proxy
