568000d4b4eff06905fbd1fc04f091426846bd7c
CarlosRauert <carlos.rauert@charite.de>
2024-11-25 17:58:06 +0100

    efesf

---
+++ GatSV/20241125_GatSV_Run5001.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+conda_init
+conda activate Gatsv
+#set variables
+DIR="/data/cephfs-1/home/users/rauertc_c/liposarcoma-wgs/WGS/HMF_GRIDSS_vcfs"
+OUTPUT_DIR="/data/cephfs-1/home/users/rauertc_c/work/GatSV/Out/GetScaled/"
+SCRIPT="/data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/GatSV/20241125_GatsV_Run.R"
+
+# Loop over Sample IDs
+#for item in $(ls "$DIR" | sed 's/\..*//' | sort -u); do
+#    # Set variable with .purple.sv.vcf.gz extension
+#    variable="${DIR}/${item}.purple.sv.vcf.gz"
+#    # Run the R script with the required arguments
+#    Rscript "$SCRIPT" "$variable" "$item" "$OUTPUT_DIR"
+#done
+
+# Loop over Sample IDs for a specified range (e.g., 1001:2000)
+start=5001
+end=5500
+
+# Extract the range of items based on the specified start and end
+count=0
+for item in $(ls "$DIR" | sed 's/\..*//' | sort -u | sed -n "${start},${end}p"); do
+    # Increment the counter
+    count=$((count + 1))
+
+    # Set variable with .purple.sv.vcf.gz extension
+    variable="${DIR}/${item}.purple.sv.vcf.gz"
+
+    # Run the R script with the required arguments
+    Rscript "$SCRIPT" "$variable" "$item" "$OUTPUT_DIR"
+
+done
