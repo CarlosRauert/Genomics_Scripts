d682a884d60d8154d7b0f0df9229e46524cf65d0
CarlosRauert <carlos.rauert@charite.de>
2024-11-26 20:23:25 +0100

    fdsfvdfvefrv

--- GatSV/20241125_GatSV_Run.sh
+++ GatSV/20241125_GatSV_Run.sh
@@ -1,3 +1,5 @@
+sbatch --ntasks=32 --mem=30G /data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/GatSV/20241125_GatSV_RunStartEnd.sh 2501 30000
+
 #set variables
 DIR="/data/cephfs-1/home/users/rauertc_c/liposarcoma-wgs/WGS/HMF_GRIDSS_vcfs"
 OUTPUT_DIR="/data/cephfs-1/home/users/rauertc_c/work/GatSV/Out/GetScaled/"
--- GatSV/20241125_GatSV_RunStartEnd.sh
+++ GatSV/20241125_GatSV_RunStartEnd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-conda_init
+/data/cephfs-1/home/users/rauertc_c/work/miniforge3/etc/profile.d/conda.sh
 conda activate Gatsv
 #set variables
 DIR="/data/cephfs-1/home/users/rauertc_c/liposarcoma-wgs/WGS/HMF_GRIDSS_vcfs"
