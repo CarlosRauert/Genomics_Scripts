3b1a43e165c54ba533b241518c1690cd6cb72d38
CarlosRauert <carlos.rauert@charite.de>
2024-11-26 23:00:48 +0100

    f<aef<af

--- GatSV/20241125_GatSV_RunStartEnd.sh
+++ GatSV/20241125_GatSV_RunStartEnd.sh
@@ -7,14 +7,6 @@ DIR="/data/cephfs-1/home/users/rauertc_c/liposarcoma-wgs/WGS/HMF_GRIDSS_vcfs"
 OUTPUT_DIR="/data/cephfs-1/home/users/rauertc_c/work/GatSV/Out/GetScaled/"
 SCRIPT="/data/cephfs-1/home/users/rauertc_c/work/Scripts_Git_Repos/Genomics_Scripts/GatSV/20241125_GatsV_Run.R"
 
-# Loop over Sample IDs
-#for item in $(ls "$DIR" | sed 's/\..*//' | sort -u); do
-#    # Set variable with .purple.sv.vcf.gz extension
-#    variable="${DIR}/${item}.purple.sv.vcf.gz"
-#    # Run the R script with the required arguments
-#    Rscript "$SCRIPT" "$variable" "$item" "$OUTPUT_DIR"
-#done
-
 # Loop over Sample IDs for a specified range (e.g., 1001:2000)
 start=$1
 end=$2
@@ -28,7 +20,12 @@ for item in $(ls "$DIR" | sed 's/\..*//' | sort -u | sed -n "${start},${end}p");
     # Set variable with .purple.sv.vcf.gz extension
     variable="${DIR}/${item}.purple.sv.vcf.gz"
     echo "Processing Count: $count, Sample ID: $item, file: $variable"
+
+    # Create a subdirectory for each sample ID to reduce contention
+    OUTPUT_SUBDIR="${OUTPUT_DIR}/${item}/"
+    mkdir -p "$OUTPUT_SUBDIR"  # Create the subdirectory if it doesn't exist
+
     # Run the R script with the required arguments
-    Rscript "$SCRIPT" "$variable" "$item" "$OUTPUT_DIR" 2>&1 > /dev/null
+    Rscript "$SCRIPT" "$variable" "$item" "$OUTPUT_SUBDIR" 2>&1 > /dev/null
 
 done
